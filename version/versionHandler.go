18883c9c658409a2b0df587b8a3b12b19caa8f1c
seungwon_lee <seungwon_lee@tmax.co.kr>
2020-11-04 08:16:21 +0000

    VersionHandler for loop Draft

--- version/versionHandler.go
+++ version/versionHandler.go
@@ -4,7 +4,7 @@ import (
 	"crypto/tls"
 	"io/ioutil"
 	"net"
-	"regexp"
+	"reflect"
 	"time"
 
 	yaml "gopkg.in/yaml.v2"
@@ -18,61 +18,14 @@ import (
 	"k8s.io/klog"
 )
 
-// ModuleInfo is a strcut for storing configMap file.
-// It must support all possible cases described in configMap.
-type ModuleInfo struct {
-	Name      string `yaml:"name"`
-	Namespace string `yaml:"namespace"`
-	Selector  struct {
-		MatchLabels struct {
-			StatusLabel  []string `yaml:"structLabel"`
-			VersionLabel []string `yaml:"versionLabel"`
-		} `yaml:"matchLabels"`
-	} `yaml:"selector"`
-	ReadinessProbe struct {
-		Exec struct {
-			Command   []string `yaml:"command"`
-			Container string   `yaml:"container"`
-		} `yaml:"exec"`
-		HTTPGet struct {
-			Path        string `yaml:"path"`
-			Port        string `yaml:"port"`
-			Scheme      string `yaml:"scheme"`
-			ServiceName string `yaml:"serviceName"`
-		} `yaml:"httpGet"`
-		TCPSocket struct {
-			Port string `yaml:"port"`
-		} `yaml:"tcpSocket"`
-	} `yaml:"readinessProbe"`
-	VersionProbe struct {
-		Exec struct {
-			Command   []string `yaml:"command"`
-			Container string   `yaml:"container"`
-		} `yaml:"exec"`
-	} `yaml:"versionProbe"`
-}
-
-// Config struct is array of ModuleInfo.
-type Config struct {
-	Modules []ModuleInfo `yaml:"modules"`
-}
-
-// Module struct is for storing result and returning to client.
-type Module struct {
-	Name    string `json:"name"`
-	Status  string `json:"status"`
-	Version string `json:"version"`
-}
-
 // Get handles ~/version get method
 func Get(res http.ResponseWriter, req *http.Request) {
 	klog.Infoln("**** GET /version")
-
 	var conf Config
 
 	// 1. READ CONFIG FILE
 	// If it runs on POD, path should be same with
-	// what declared on volume in yaml file ( /config/module.config )
+	// what declared on volume in yaml file.
 	yamlFile, err := ioutil.ReadFile("/config/module.config")
 	if err != nil {
 		klog.Errorln(err)
@@ -88,7 +41,6 @@ func Get(res http.ResponseWriter, req *http.Request) {
 	for idx, mod := range conf.Modules {
 		klog.Infoln("Module Name = ", mod.Name)
 		result[idx].Name = mod.Name
-
 		// 2. GET STATUS
 		var labels string
 		for i, label := range mod.Selector.MatchLabels.StatusLabel {
@@ -100,19 +52,25 @@ func Get(res http.ResponseWriter, req *http.Request) {
 		}
 		podList, exist := k8sApiCaller.GetPodListByLabel(labels, mod.Namespace)
 
+		ps := NewPodStatus()
+
 		if !exist {
 			klog.Errorln("cannot found pods using given label")
 			result[idx].Status = "Not found"
 		} else if mod.ReadinessProbe.Exec.Command != nil {
 			// by exec command
-			stdout, stderr, err := k8sApiCaller.ExecCommand(podList.Items[0], mod.ReadinessProbe.Exec.Command, mod.ReadinessProbe.Exec.Container)
-			output := stderr + stdout
-			if err != nil {
-				klog.Errorln(mod.Name, " exec command error : ", err)
-				result[idx].Status = "Command error"
-			} else {
-				result[idx].Status = strings.Trim(output, "\n")
+			for j := range podList.Items {
+				stdout, stderr, err := k8sApiCaller.ExecCommand(podList.Items[j], mod.ReadinessProbe.Exec.Command, mod.ReadinessProbe.Exec.Container)
+				output := stderr + stdout
+
+				if err != nil {
+					klog.Errorln(mod.Name, " exec command error : ", err)
+					ps.Data["Command error"]++
+				} else {
+					ps.Data[output]++
+				}
 			}
+			result[idx].Status = AppendStatusResult(*ps)
 		} else if mod.ReadinessProbe.HTTPGet.Path != "" {
 			// by HTTP
 			// This code can only work on POD
@@ -137,33 +95,41 @@ func Get(res http.ResponseWriter, req *http.Request) {
 			}
 		} else if mod.ReadinessProbe.TCPSocket.Port != "" {
 			// by Port
-			host := podList.Items[0].Status.PodIP
 			port := mod.ReadinessProbe.TCPSocket.Port
 			timeout := time.Second
-			conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
-			defer conn.Close()
-			if err != nil {
-				klog.Errorln(err)
-				result[idx].Status = "Not ready"
-			} else {
-				result[idx].Status = "Ready"
+			for j := range podList.Items {
+				host := podList.Items[j].Status.PodIP
+
+				conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+				defer conn.Close()
+				if err != nil {
+					klog.Errorln(err)
+					ps.Data["Not ready"]++
+				} else {
+					ps.Data["Ready"]++
+				}
 			}
+			result[idx].Status = AppendStatusResult(*ps)
 		} else {
 			// by Status.Phase
-			result[idx].Status = string(podList.Items[0].Status.Phase)
+			for j := range podList.Items {
+				ps.Data[string(podList.Items[j].Status.Phase)]++
+			}
+			result[idx].Status = AppendStatusResult(*ps)
 		}
 
 		// 3. GET VERSION
-		labels = ""
-		for i, label := range mod.Selector.MatchLabels.VersionLabel {
-			if i == 0 {
-				labels = label
-			} else {
-				labels += ", " + label
+		if !(reflect.DeepEqual(mod.Selector.MatchLabels.StatusLabel, mod.Selector.MatchLabels.VersionLabel)) {
+			labels = ""
+			for i, label := range mod.Selector.MatchLabels.VersionLabel {
+				if i == 0 {
+					labels = label
+				} else {
+					labels += ", " + label
+				}
 			}
+			podList, exist = k8sApiCaller.GetPodListByLabel(labels, mod.Namespace)
 		}
-
-		podList, exist = k8sApiCaller.GetPodListByLabel(labels, mod.Namespace)
 
 		if !exist {
 			klog.Errorln("cannot found pods using given label")
@@ -189,21 +155,4 @@ func Get(res http.ResponseWriter, req *http.Request) {
 
 	// encode to JSON format and response
 	res = util.SetResponse(res, "", result, http.StatusOK)
-}
-
-// ParsingVersion parses version using regular expression
-func ParsingVersion(str string) string {
-	isLatest, err := regexp.MatchString("latest", str)
-	if err != nil {
-		klog.Errorln(err)
-	} else if isLatest {
-		return "latest"
-	}
-
-	r, err := regexp.Compile("[a-z]*[A-Z]*[0-9]*\\.[0-9]*\\.[0-9]*")
-	if err != nil {
-		klog.Errorln(err)
-	}
-
-	return r.FindString(str)
 }
