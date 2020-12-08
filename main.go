a7ebfb9dbbc8751d63c345a5a5b8641ec6bc45ac
dnxorjs1 <taegeon_woo@tmax.co.kr>
2020-12-08 20:13:54 +0900

    api-server logging 시스템 추가

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+
 	alert "github.com/tmax-cloud/hypercloud-api-server/alert"
 	metering "github.com/tmax-cloud/hypercloud-api-server/metering"
 	"github.com/tmax-cloud/hypercloud-api-server/namespace"
@@ -15,69 +22,46 @@ import (
 	"github.com/robfig/cron"
 )
 
-const (
-	TEST = "<!DOCTYPE html>\r\n" +
-		"<html lang=\"en\">\r\n" +
-		"<head>\r\n" +
-		"    <meta charset=\"UTF-8\">\r\n" +
-		"    <title>HyperCloud 서비스 신청 승인 완료</title>\r\n" +
-		"</head>\r\n" +
-		"<body>\r\n" +
-		"<div style=\"border: #c5c5c8 0.06rem solid; border-bottom: 0; width: 42.5rem; height: 53.82rem; padding: 0 1.25rem\">\r\n" +
-		"    <header>\r\n" +
-		"        <div style=\"margin: 0;\">\r\n" +
-		"            <p style=\"font-size: 1rem; font-weight: bold; color: #333333; line-height: 3rem; letter-spacing: 0; border-bottom: #c5c5c8 0.06rem solid;\">\r\n" +
-		"                HyperCloud 서비스 신청 승인 완료\r\n" +
-		"            </p>\r\n" +
-		"        </div>\r\n" +
-		"    </header>\r\n" +
-		"    <section>\r\n" +
-		"        <figure style=\"text-align: center;\">\r\n" +
-		"            <img style=\"margin: 0.94rem 0;\"\r\n" +
-		"                 src=\"cid:trial-approval\">\r\n" +
-		"        </figure>\r\n" +
-		"        <div style=\"width: 35.70rem; margin: 0 2.75rem;\">\r\n" +
-		"            <p style=\"font-size: 1.5rem; font-weight: bold; line-height: 3rem;\">\r\n" +
-		"                축하합니다.\r\n" +
-		"            </p>\r\n" +
-		"            <p style=\"line-height: 1.38rem;\">\r\n" +
-		"                고객님의 Trial 서비스 신청이 성공적으로 승인되었습니다. <br>\r\n" +
-		"                지금 바로 티맥스의 소프트웨어와 검증을 거친 오픈소스 서비스를 결합한 클라우드 플랫폼, <br>\r\n" +
-		"                HyperCloud를 이용해 보세요. <br>\r\n" +
-		"                <br>\r\n" +
-		"                네임스페이스 이름 : <span style=\"font-weight: 600;\">%%NAMESPACE_NAME%%</span> <br>\r\n" +
-		"                Trial 기한 : %%TRIAL_START_TIME%% ~ %%TRIAL_END_TIME%% <br>\r\n" +
-		"                <br>\r\n" +
-		"                리소스 정보 <br>\r\n" +
-		"                -CPU : 1 Core <br>\r\n" +
-		"                -Memory : 4 GIB <br>\r\n" +
-		"                -Storage : 4 GIB <br>\r\n" +
-		"                <br>\r\n" +
-		"<!--                <span style=\"font-weight: 600;\">승인사유</span> <br>-->\r\n" +
-		"                <br>\r\n" +
-		"\r\n" +
-		"                감사합니다. <br>\r\n" +
-		"                TmaxCloud 드림.\r\n" +
-		"            </p>\r\n" +
-		"            <p style=\"margin: 3rem 0;\">\r\n" +
-		"                <a href=\"https://console.tmaxcloud.com\">Tmax Console 바로가기 ></a>\r\n" +
-		"            </p>\r\n" +
-		"        </div>\r\n" +
-		"    </section>\r\n" +
-		"</div>\r\n" +
-		"<footer style=\"background-color: #3669B3; width: 45.12rem; height: 1.88rem; font-size: 0.75rem; color: #FFFFFF; display: flex;\r\n" +
-		"    align-items: center; justify-content: center;\">\r\n" +
-		"    <div>\r\n" +
-		"        COPYRIGHT2020. TMAX A&C., LTD. ALL RIGHTS RESERVED\r\n" +
-		"    </div>\r\n" +
-		"</footer>\r\n" +
-		"</body>\r\n" +
-		"</html>"
-)
-
 func main() {
-	// Metering Cron Job
+	// For Log file
+	klog.InitFlags(nil)
+	flag.Set("logtostderr", "false")
+	flag.Set("alsologtostderr", "false")
+	flag.Parse()
+
+	file, err := os.OpenFile(
+		"./logs/api-server.log",
+		os.O_CREATE|os.O_RDWR|os.O_TRUNC,
+		os.FileMode(0644),
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	w := io.MultiWriter(file, os.Stdout)
+	klog.SetOutput(w)
+
+	// Logging Cron Job
 	cronJob := cron.New()
+	cronJob.AddFunc("1 0 0 * * ?", func() {
+		input, err := ioutil.ReadFile("./logs/api-server.log")
+		if err != nil {
+			klog.Error(err)
+			return
+		}
+		err = ioutil.WriteFile("./logs/api-server"+time.Now().AddDate(0, 0, -1).Format("2006-01-02")+".log", input, 0644)
+		if err != nil {
+			klog.Error(err, "Error creating", "./logs/api-server")
+			fmt.Println(err)
+			return
+		}
+		klog.Info("Log BackUp Success")
+		os.Truncate("./logs/api-server.log", 0)
+		file.Seek(0, os.SEEK_SET)
+	})
+
+	// Metering Cron Job
 	cronJob.AddFunc("0 */5 * ? * *", metering.MeteringJob)
 	cronJob.Start()
 
@@ -92,6 +76,8 @@ func main() {
 
 	// HTTP Server Start
 	klog.Info("Starting Hypercloud-Operator-API server...")
+	klog.Flush()
+
 	if err := http.ListenAndServe(":80", mux); err != nil {
 		klog.Errorf("Failed to listen and serve Hypercloud-Operator-API server: %s", err)
 	}
