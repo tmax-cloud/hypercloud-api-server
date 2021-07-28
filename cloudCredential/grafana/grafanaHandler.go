b0a1bb58926fa72f47e7ede0a5ba3a421221c512
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-07-28 14:22:25 +0900

    query 기본 틀 작성

--- cloudCredential/grafana/grafanaHandler.go
+++ cloudCredential/grafana/grafanaHandler.go
@@ -2,10 +2,22 @@ package grafana
 
 import (
 	"net/http"
+	"time"
 
-	//_ "github.com/grafana/grafana_plugin_model/go/datasource"
 	"github.com/tmax-cloud/hypercloud-api-server/util"
+	"k8s.io/klog"
 )
+
+type QueryResponse struct {
+	Target string `json:"target"`
+	//Datapoints []DataPoint `json:"datapoints"`
+	Datapoints [][]float32 `json:"datapoints"`
+}
+
+// type DataPoint struct {
+// 	Value         float32 `json:"value"`
+// 	Unixtimestamp int64   `json:"unixtimestamp"`
+// }
 
 func Get(res http.ResponseWriter, req *http.Request) {
 	util.SetResponse(res, "Test Success", nil, http.StatusOK)
@@ -24,6 +36,19 @@ func Search(res http.ResponseWriter, req *http.Request) {
 
 func Query(res http.ResponseWriter, req *http.Request) {
 
+	var qr []QueryResponse
+
+	var temp QueryResponse
+	temp.Target = "billing_by_account"
+	time := time.Now().Unix()
+
+	temp.Datapoints = append(temp.Datapoints, []float32{0.5, float32(time)})
+
+	qr = append(qr, temp)
+
+	klog.Infoln("qr =", qr)
+
+	util.SetResponse(res, "", qr, http.StatusOK)
 }
 
 func Annotations(res http.ResponseWriter, req *http.Request) {
