ace2e4c70659786db24739c02ac424bda49039dc
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-12-06 15:21:21 +0900

    [mod] kafka DNS 주소만 고려

--- util/consumer/kafkaConsumer.go
+++ util/consumer/kafkaConsumer.go
@@ -100,15 +100,15 @@ func HyperauthConsumer() {
 	var kafka1_addr string
 	var kafka2_addr string
 	var kafka3_addr string
-	if os.Getenv("kafka1_addr") != "DNS" {
-		kafka1_addr = os.Getenv("kafka1_addr")
-		kafka2_addr = os.Getenv("kafka2_addr")
-		kafka3_addr = os.Getenv("kafka3_addr")
-	} else {
-		kafka1_addr = "kafka-1.hyperauth:9092"
-		kafka2_addr = "kafka-2.hyperauth:9092"
-		kafka3_addr = "kafka-3.hyperauth:9092"
-	}
+	// if os.Getenv("kafka1_addr") != "DNS" {
+	// 	kafka1_addr = os.Getenv("kafka1_addr")
+	// 	kafka2_addr = os.Getenv("kafka2_addr")
+	// 	kafka3_addr = os.Getenv("kafka3_addr")
+	// } else {
+	kafka1_addr = "kafka-1.hyperauth:9092"
+	kafka2_addr = "kafka-2.hyperauth:9092"
+	kafka3_addr = "kafka-3.hyperauth:9092"
+	// }
 
 	client, err := sarama.NewConsumerGroup([]string{kafka1_addr, kafka2_addr, kafka3_addr}, consumerGroupId, consumerConfig)
 	if err != nil {
