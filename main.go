3b5dae660db66dac6d440f50164f171ad711457e
Seungwon Lee <seungwon_lee@tmax.co.kr>
2022-05-17 09:51:03 +0900

    [etc] leader election 최대 유휴시간 10초로 감소

--- main.go
+++ main.go
@@ -755,8 +755,8 @@ func runLeaderElection(lock *resourcelock.LeaseLock, ctx context.Context, id str
 	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
 		Lock:            lock,
 		ReleaseOnCancel: true,
-		LeaseDuration:   15 * time.Second,
-		RenewDeadline:   10 * time.Second,
+		LeaseDuration:   10 * time.Second,
+		RenewDeadline:   5 * time.Second,
 		RetryPeriod:     2 * time.Second,
 		Callbacks: leaderelection.LeaderCallbacks{
 			OnStartedLeading: func(c context.Context) {
