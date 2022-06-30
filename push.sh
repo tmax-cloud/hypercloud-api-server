5d252e9cd9476d1ab16656dd40af860deb99cc6d
Seungwon Lee <seungwon_lee@tmax.co.kr>
2022-06-30 17:19:38 +0900

    테스트 편의성을 위한 이미지 생성/삭제 스크립트 작성

--- push.sh
+++ push.sh
@@ -1,5 +1,22 @@
-#!/bin/sh
-img=192.168.9.12:5000/hypercloud-api-server:v3.59
-docker rmi $img 
+#!/bin/bash
+
+# $1={version}
+if [ -z "$1" ]
+then
+	version=0
+else
+	version=$1
+fi
+
+if [ -z "$2" ]
+then
+	author='-dev-swlee'
+else
+	author=$2
+fi
+
+img=192.168.9.12:5000/hypercloud-api-server-test$author:v$version
+echo "Image = $img"
 docker build -t $img  . 
-docker push $img 
+docker push $img
+docker rmi $img 
